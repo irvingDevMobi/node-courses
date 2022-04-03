b09eb246403f02f750f5343e91b8f0dd61dadeb6
Irving Dev <irving.dev.mobi@gmail.com>
2022-04-03 13:26:52 -0500

    functions scope

---
+++ closure-and-scopes/src/scope/functions.js
@@ -0,0 +1,19 @@
+const printFruits = () => {
+    var fruit = 'apple'
+    console.log(fruit)
+}
+
+printFruits()
+
+const anotherFunction = () => {
+    var xVar = 1
+    var xVar = 2
+    
+    let yLet = 3
+    // let yLet = 4 Let help us to redeclare a property
+
+    console.log(xVar)
+    console.log(yLet)
+}
+
+anotherFunction()
